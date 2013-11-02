7df27cbb138a34d0cc29696e773f8877407f34b9
Wojciech Migda <wojtek.golf@interia.pl>
2013-11-02 18:59:02 +0100

    boilerplate

---
+++ tests/test_foo.hpp
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Wojciech Migda
+ * All rights reserved
+ * Distributed under the terms of the GNU LGPL v3
+ *******************************************************************************
+ *
+ * Filename: test_foo.hpp
+ *
+ * Description:
+ *      description
+ *
+ * Authors:
+ *          Wojciech Migda (wm)
+ *
+ *******************************************************************************
+ * History:
+ * --------
+ * Date         Who  Ticket     Description
+ * ----------   ---  ---------  ------------------------------------------------
+ * 2013-06-27   wm              Initial version
+ *
+ ******************************************************************************/
+
+#include <cxxtest/TestSuite.h>
+#include <gmock/gmock.h>
+#include <memory>
+
+
+class TestFoo : public CxxTest::TestSuite
+{
+private:
+
+
+public:
+
+TestFoo()
+{
+}
+~TestFoo()
+{
+}
+
+void test_111(void)
+{
+}
+
+};
